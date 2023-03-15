7de8eaa74d52ecef2ed2c666ab5ad261af60e0f4
Nghia Nguyen <nghia.nguyen@bmbsoft.com.vn>
2023-03-15 17:57:52 +0700

    add

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,55 @@
 import React, { useState, useEffect } from 'react';
 import { Loader, Card, FormField } from '../components';
 
-const Home = () => {
+const RenderCards = ({ data, title }) => {
+  if (data?.length > 0) { return data.map((post) => <Card key={post._id} {...post} />) }
+
   return (
-    <div>Home</div>
+    <h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>{title}</h2>
+  )
+}
+const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState([null]);
+  const [searchText, setsearchText] = useState('');
+  return (
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>The comunity Showcase</h1>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w-[500px]'>Browse through a colection of imaginative and visually stunning images generated DALL-E AI</p>
+      </div>
+
+      <div className='mt-16'>
+        <FormField />
+      </div>
+
+      <div className='mt-10'>
+        {loading ? (
+          <div className='flex justify-center items-center'>
+            <Loader />
+          </div>
+        ) : (
+          <>
+            {searchText && (
+              <h2 className='font-medium text-[#666e75] text-xl mb-3'>
+                Showing results for <span className='text-[#222328]'>{searchText}</span>
+              </h2>
+            )}
+            <div className='grid lg:grid-col-4 sm:grid-col-3 xs:grid-col-2 grid-col-1 gap-3'>
+              {searchText ? (
+                <RenderCards
+                  data={[]}
+                  title="No search results found" />
+              ) : (
+                <RenderCards
+                  data={[]}
+                  title="No posts found" />
+              )}
+            </div>
+          </>
+        )}
+      </div>
+    </section>
   )
 }
 
